0df08df86388f9c4bafc3468bfc09263e12a02cd
SuleimanKh97 <suleiman97kh@gmail.com>
2025-01-20 09:13:44 +0300

    proplem solving

---
+++ day3-20-01-25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3_20_01_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountVowels();
+        }
+
+        static void CountVowels()
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+            int count = 0;
+            foreach (char c in input)
+            {
+                if ("aeiou".Contains(c))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Number of vowels: " + count);
+        }
+        
+    }
+}
