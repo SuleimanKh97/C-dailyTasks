434dde02e1241e777723bf3814acabf7b05ec9db
SuleimanKh97 <suleiman97kh@gmail.com>
2025-01-21 17:11:04 +0300

    all done

--- day3-21-01-25/Program.cs
+++ day3-21-01-25/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace day3_21_01_25
 {
@@ -23,7 +24,7 @@ namespace day3_21_01_25
             age = 0;
             studentId = 0;
         }
-        public Student(string studentName , double age , int studentId)  //Constructor
+        public Student(string studentName, double age, int studentId)  //Constructor
         {
             this.studentName = studentName;
             this.age = age;
@@ -45,7 +46,7 @@ namespace day3_21_01_25
     }
     internal class Program
     {
-       
+
         static void Main(string[] args)
         {
             Student student1 = new Student(); // object we create the data manually
@@ -69,12 +70,22 @@ namespace day3_21_01_25
 
         }
 
-        public static void getDetails(Student student) // method for print to console student data
-        {
-            Console.WriteLine("Student Name: " + student.studentName);
-            Console.WriteLine("Student Age: " + student.age);
-            Console.WriteLine("Student ID: " + student.studentId);
-            Console.WriteLine("******************************************************");
-        }
+        
+        //1- A class is a blueprint for creating objects (a particular data structure), providing initial values for state (property or fields), and implementations of behavior (member functions or methods). The user-defined objects are created using the class keyword.
+        //2- Object: An instance of a class containing specific data and behavior defined by the class.
+
+        //3- Difference: A class is a blueprint, while an object is a specific instance of that blueprint.
+
+        //4- OOP Principles: Encapsulation, Inheritance, Polymorphism, and Abstraction.
+
+        //5- Property: A class member that provides a flexible way to access and modify fields, often with get and set methods.
+
+        //6- Field: A variable defined within a class to store data.
+
+        //7- Constructor: A special method used to initialize objects of a class.
+
+        //8- Encapsulation: Restricting access to an object's data and exposing only necessary parts through methods or properties.
+
+
     }
 }
